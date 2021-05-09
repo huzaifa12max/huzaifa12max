ee77907c8e705d337d3cd7cd9c87ce0e4c4ce6f0
huzaifa12max <81131294+huzaifa12max@users.noreply.github.com>
2021-05-09 13:32:33 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <title>repl.it</title>
+    <link href="style.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+		<a class="button" href="REDIRECT LINK" target="_blank" role="button">YOUR TEXT HERE</a>
+		<div>
+  </body>
+</html>
